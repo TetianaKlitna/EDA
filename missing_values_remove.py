28ca897df2cdd8efbcbbfbd498add71d024e8b71
TetianaKlitna <146033253+TetianaKlitna@users.noreply.github.com>
2024-03-20 15:23:48 -0700

    Add files via upload

---
+++ missing_values_remove.py
@@ -0,0 +1,21 @@
+# Count the number of missing values in each column
+print(planes.isna().sum())
+
+# Find the five percent threshold
+threshold = len(planes) * 0.05
+
+# Create a filter
+cols_to_drop = planes.columns[planes.isna().sum() <= threshold]
+
+# Drop missing values for columns below the threshold
+planes.dropna(subset=cols_to_drop, inplace=True)
+
+print(planes.isna().sum())
+##################################################
+# Check the values of the Additional_Info column
+print(planes["Additional_Info"].value_counts())
+
+# Create a box plot of Price by Airline
+sns.boxplot(data=planes, x="Airline", y="Price")
+
+plt.show()
