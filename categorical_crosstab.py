6f4575d7182869e5b2556937174b067dd7fd8a6a
TetianaKlitna <146033253+TetianaKlitna@users.noreply.github.com>
2024-03-20 23:44:52 -0700

    Add files via upload

---
+++ categorical_crosstab.py
@@ -0,0 +1,18 @@
+# Print the relative frequency of Job_Category
+print(salaries["Job_Category"].value_counts(normalize = True))
+
+########################################################
+
+# Cross-tabulate Company_Size and Experience
+print(pd.crosstab(salaries["Company_Size"], salaries["Experience"]))
+
+########################################################
+
+# Cross-tabulate Job_Category and Company_Size
+print(pd.crosstab(salaries["Job_Category"], salaries["Company_Size"]))
+
+########################################################
+
+# Cross-tabulate Job_Category and Company_Size
+print(pd.crosstab(salaries["Job_Category"], salaries["Company_Size"],
+            values=salaries["Salary_USD"], aggfunc="mean"))
